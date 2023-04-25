01f8d9faa46a03729712a9e6f5210deff0146fed
matheusbudin <matheusbudin@hotmail.com>
2023-04-25 20:04:20 -0300

    Airflow Ingest data from an API

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # AirflowProject-with-astro-python-sdk
+
+This is a airflow project that ingests data from any API that you might want.
+
+First things first, you will have to install astro python SDK in your machine,
+Assuming that you already have airflow installed, follow the steps ahead:
+
+* follow this video: https://www.loom.com/share/7589687641e34c67b19572cd6b8ed903
+
+#dependecies needed:
+
+- [Astro Python SDK instalation] (https://github.com/astronomer/astro-sdk)
+
+*To test it:
+1. change the line 29, overwriting with an API URL of youre choice;
+2. Set your connection to a database of your choice.
+
+For any further questions please let me know, i will be glad to help :)
+reach me on my linkedin:  https://linkedin.com/in/matheusbudin
---
+++ main.py
@@ -0,0 +1,46 @@
+import astro
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+# define the default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 4, 25),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# define the DAG object
+dag = DAG(
+    'my_api_task',
+    default_args=default_args,
+    description='A DAG to fetch data from an API and store it in a database',
+    schedule_interval='0 0 * * *', # run the task every day at midnight
+)
+
+# define the Python function that will be used as the task
+def fetch_and_store_data():
+    # make the API request using Astro Python SDK
+    api_data = astro.requests.get('https://api.example.com/data')
+    
+    # store the data in the database using Astro Python SDK
+    db_conn = astro.connect('postgresql://username:password@host:port/database')
+    cursor = db_conn.cursor()
+    cursor.execute('INSERT INTO my_table (data) VALUES (%s)', (api_data,))
+    db_conn.commit()
+    db_conn.close()
+
+# define the operator that will run the task
+run_task = PythonOperator(
+    task_id='fetch_and_store_data',
+    python_callable=fetch_and_store_data,
+    dag=dag,
+)
+
+# set the dependencies of the task
+run_task
